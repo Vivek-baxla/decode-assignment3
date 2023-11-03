31f1e0c20d6d9f6631b7d331ad9bbbaf254d33f9
Vivek-baxla <144560722+Vivek-baxla@users.noreply.github.com>
2023-11-04 00:43:22 +0530

    solved assignments are added

---
+++ greatestOf2.cpp
@@ -0,0 +1,14 @@
+# include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter 1st number : ";
+    cin>>a;
+    cout<<"Enter 2nd number : ";
+    cin>>b;
+    if(a>b){
+        cout<<a<<" is greatest";
+    } else{
+        cout<<b<<" is greatest";
+    }
+}
